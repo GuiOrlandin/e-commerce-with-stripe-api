acb7b6ac81bb0db2b8847386b1ec96752ae42c57
Guilherme Orlandin <110955432+GuiOrlandin@users.noreply.github.com>
2024-09-29 18:50:02 -0300

    main ts changed

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,22 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     rawBody: true,
   });
-  app.enableCors({
-    origin: ['http://localhost:5173'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
   });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const port = process.env.PORT || 3333;
