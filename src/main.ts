7d9ac4c1798e944dbba4c6eed7131f0027f26553
Guilherme Orlandin <110955432+GuiOrlandin@users.noreply.github.com>
2024-09-29 16:14:58 -0300

    cors updated

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     rawBody: true,
   });
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://e-commerce-with-stripe-9gq4ynunt-guiorlandins-projects.vercel.app',
+      'http://localhost:3000',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const port = process.env.PORT || 3333;
